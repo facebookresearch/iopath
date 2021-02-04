6c351b9aaa658a783802e1c40f8fa29eb6589601
Eric Lou <ericlou@fb.com>
2021-02-04 09:29:52 -0800

    ioPath simple imports
    Summary:
    **Summary:** Modified `__init__.py` files for simpler import statements.
    
    **Reason:**
    ```
    # I found myself wanting to do this...
    from iopath import PathManager
    
    # Instead of...
    from iopath.common.file_io import PathManager
    ```
    
    **What do you think?**
    
    Reviewed By: calebho
    
    Differential Revision: D26234673
    
    fbshipit-source-id: 94c4f806c077b1943cf82990cedb84d60c60bb0b

--- iopath/__init__.py
+++ iopath/__init__.py
@@ -1,5 +1,19 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
+from iopath.common import (
+    LazyPath,
+    PathManager,
+    get_cache_dir,
+    file_lock,
+)
+
+__all__ = [
+    "LazyPath",
+    "PathManager",
+    "get_cache_dir",
+    "file_lock",
+]
+
 # This line will be programatically read/write by setup.py.
 # Leave them at the bottom of this file and don't touch them.
 __version__ = "0.1.3"
