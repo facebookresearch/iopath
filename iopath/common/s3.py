33c025554c9c0101bcbc5f5a059eb92e720450ff
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-01-26 21:21:12 -0800

    suppress errors in `fair_infra/data/iopath`
    Differential Revision: D53150909
    
    fbshipit-source-id: e02c31c4789afaf480f9bab9c3cce1b8ae76eb96

--- iopath/common/s3.py
+++ iopath/common/s3.py
@@ -722,6 +722,8 @@ class S3ChunkReadIO(io.BufferedIOBase):
 
     # pyre-fixme[15]: `write` overrides method defined in `BufferedIOBase`
     #  inconsistently.
+    # pyre-fixme[14]: `write` overrides method defined in `BufferedIOBase`
+    #  inconsistently.
     def write(self, b: Union[bytes, bytearray]) -> Optional[int]:
         """
         Write bytes b to in-memory buffer, return number written.
